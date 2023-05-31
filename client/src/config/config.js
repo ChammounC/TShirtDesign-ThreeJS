1fbaa2a055671f685d4055d212c7a23f3f452c81
ChammounC <chingchakhap11@gmail.com>
2023-06-01 03:35:20 +0530

    modified config prod url, add .env into gitignore

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,10 @@
+
 const config = {
   development: {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://project-threejs-ai-mo69.onrender.com/api/v1/dalle",
+    backendUrl: import.meta.env.VITE_URL,
   },
 };
 
