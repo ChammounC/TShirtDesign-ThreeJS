6c7ef08b0f7bb66a5a4694572a4267a1aac6b5af
ChammounC <chingchakhap11@gmail.com>
2023-06-01 12:59:46 +0530

    resolve merge

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -4,7 +4,6 @@ import {  useRef } from 'react';
 import { AccumulativeShadows,RandomizedLight } from '@react-three/drei';
 
 const Backdrop = () => {
-  console.log();
     const shadows=useRef();
 
   return (
